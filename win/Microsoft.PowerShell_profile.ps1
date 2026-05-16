13b661817d212b311438d383d392867b130d3c65
JB Ladera <minidomou@gmail.com>
2026-05-16 15:24:12 -0500

    add cmdlet to set/clear envionment variables (#7)
    * add function for setting env vars
    
    * update current session

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -225,6 +225,7 @@ function Invoke-Starship-PreCommand {
 
 function Test-Port {
     param(
+        [Parameter(Mandatory)]
         [int]$Port
     )
 
@@ -248,6 +249,7 @@ function Test-Port {
 
 function Invoke-RemoteScript {
     param(
+        [Parameter(Mandatory)]
         [String]$Url
     )
 
@@ -261,6 +263,39 @@ function Invoke-RemoteScript {
 
 function Optimize-Path {
     Invoke-RemoteScript "https://raw.githubusercontent.com/jonasgeiler/pathix/0dfc68071f9494c6a7e8cf13eace712902eb743b/pathix.ps1"
+}
+
+function Set-EnvVar {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [string]$Value,
+
+        [ValidateSet("User", "Machine")]
+        [string]$Scope = "User"
+    )
+    
+    $currentValue = [System.Environment]::GetEnvironmentVariable($Name, $Scope)
+    Write-Host "Current value of environment variable '$Name' for $Scope scope: '$currentValue'" -ForegroundColor Yellow
+
+    if ([string]::IsNullOrEmpty($Value)) {
+        if ([string]::IsNullOrEmpty($currentValue)) {
+            Write-Host "Environment variable '$Name' does not exist for $Scope scope. Nothing to delete." -ForegroundColor Yellow
+            return
+        }
+
+        [System.Environment]::SetEnvironmentVariable($Name, $null, $Scope)
+        [System.Environment]::SetEnvironmentVariable($Name, $null, "Process")
+
+        Write-Host "Environment variable '$Name' deleted for $Scope scope." -ForegroundColor Green
+        return
+    }
+
+    [System.Environment]::SetEnvironmentVariable($Name, $Value, $Scope)
+    [System.Environment]::SetEnvironmentVariable($Name, $Value, "Process")
+
+    Write-Host "Environment variable '$Name' set for $Scope scope." -ForegroundColor Green
 }
 
 gh completion -s powershell | Out-String | Invoke-Expression
